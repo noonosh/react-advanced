cfdd707697aaed91c98817c8a847181972057aac
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-06-10 16:47:40 +0500

    API request and render users list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Movie from './hoc/Movie.jsx';
-import Counter from './hooks/Counter';
+import Users from './hooks/Users.jsx';
 
 function App() {
-	return <Counter />;
+	return <Users />;
 }
 
 export default App;
